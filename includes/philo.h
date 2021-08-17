4edd37546b75c2f93f637b037f6c69135aa4c439
clbouche <57404773+clbouche@users.noreply.github.com>
2021-08-17 12:49:35 +0200

    print time OK

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:02:02 by clbouche          #+#    #+#             */
-/*   Updated: 2021/08/13 15:12:08 by clbouche         ###   ########.fr       */
+/*   Updated: 2021/08/17 12:46:09 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # define ERR_NB_ARGS "The execution of the program requires 5 or 6 parameters"
 # define ERR_NB_PHILO "No philosopher wants to eat"
 # define ERR_INIT "Something went wrong with initialization"
-# define ERR_MALLOC "Malloc returned NULL value" 
+# define ERR_MALLOC "Malloc returned NULL value"
 
 /*
 ** LIBRAIRIES
@@ -40,12 +40,13 @@
 
 typedef struct s_thread_data
 {
-	long int	start_time;
-	int			nb_of_philo;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			nb_meals_per_philo;
+	int				nb_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nb_meals_per_philo;
+	long long		start_time;
+	pthread_mutex_t	message;
 }				t_thread_data;
 
 struct	s_params;
@@ -55,7 +56,7 @@ typedef struct s_philo
 	pthread_t		thread;
 	pthread_mutex_t	*right_fork;
 	pthread_mutex_t	left_fork;
-	struct s_params	*params;
+	t_thread_data	*data;
 	int				id;
 }				t_philo;
 
@@ -63,7 +64,6 @@ typedef struct s_params
 {
 	t_philo			*philo;
 	t_thread_data	data;
-	pthread_mutex_t	message;
 }				t_params;
 
 /*
@@ -71,11 +71,11 @@ typedef struct s_params
 */
 
 void		parsing(int argc, char **argv, t_params *params);
-void		create_threads(t_params *params);
+int			create_threads(t_params *params);
 int			ft_atoi(const char *str);
 void		ft_error(char *error);
 void		philo_message(t_philo *philo, char *msg);
-long int	actual_time();
+long long   get_time(void);
 void		ft_usleep(long int time_to_go);
 
 #endif
